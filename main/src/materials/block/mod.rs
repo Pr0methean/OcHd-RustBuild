124359e99c007ea6ecb37574955236fa9d5f0c71
Chris Hennick <hennickc@amazon.com>
2023-04-18 17:55:20 -0700

    Translate command blocks

--- main/src/materials/block/mod.rs
+++ main/src/materials/block/mod.rs
@@ -1,4 +1,5 @@
 use crate::group;
+use crate::materials::block::indestructible::INDESTRUCTIBLE_BLOCKS;
 
 mod axe;
 mod bare_hand;
@@ -10,4 +11,4 @@ mod shears;
 mod shovel;
 
 group!(ALL_BLOCKS = axe::AXE_BLOCKS, pickaxe::PICKAXE_BLOCKS, shovel::SHOVEL_BLOCKS,
-        shears::SHEAR_BLOCKS, hoe::HOE_BLOCKS);
+        shears::SHEAR_BLOCKS, hoe::HOE_BLOCKS, INDESTRUCTIBLE_BLOCKS);
