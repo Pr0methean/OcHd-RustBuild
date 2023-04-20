70a65708b6c04b43c3d4a3a1344ea2a75cd090fb
Chris Hennick <hennickc@amazon.com>
2023-04-19 17:22:31 -0700

    Bug fix? Use hardlinks everywhere

--- main/src/texture_base/material.rs
+++ main/src/texture_base/material.rs
@@ -207,7 +207,7 @@ pub struct CopiedMaterial {
 impl Material for CopiedMaterial {
     fn get_output_tasks(&self) -> Vec<FileOutputTaskSpec> {
         vec![FileOutputTaskSpec::Symlink {
-            original: self.source.get_path(),
+            original: Box::new(self.source.to_owned()),
             link: name_to_out_path(self.name)
         }]
     }
