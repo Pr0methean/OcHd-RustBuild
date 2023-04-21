f0408f02d78e115a5ee326917f833f0b32a77477
Chris Hennick <hennickc@amazon.com>
2023-04-20 20:22:19 -0700

    Refactor: don't need to mkdir in rmdir task, since other IO tasks will do it if needed

--- main/src/main.rs
+++ main/src/main.rs
@@ -91,14 +91,13 @@ async fn main() {
     info!("Using {:?} pixels per tile", tile_size);
     let start_time = Instant::now();
 
-    let (send_mkdir_done, await_mkdir_done) = channel();
+    let (send_rmdir_done, await_rmdir_done) = channel();
     let copy_metadata = tokio::spawn(async {
         let result = remove_dir_all(&*OUT_DIR);
         if result.is_err_and(|err| err.kind() != NotFound) {
             panic!("Failed to delete old output directory");
         }
-        create_dir(&*OUT_DIR).expect("Failed to create output directory");
-        send_mkdir_done.send(()).expect("send_mkdir_done already sent");
+        send_rmdir_done.send(()).expect("send_mkdir_done already sent");
         copy_metadata(&(METADATA_DIR.to_path_buf()));
     });
     let output_tasks = materials::ALL_MATERIALS.get_output_tasks();
@@ -148,7 +147,7 @@ async fn main() {
     let mut components: Vec<Vec<CloneableLazyTask<()>>> = component_map.into_values().collect();
     components.sort_by_key(Vec::len);
     let components: Vec<CloneableLazyTask<()>> = components.into_iter().flatten().collect();
-    await_mkdir_done.await.expect("Failed to create or delete output directory");
+    await_rmdir_done.await.expect("Failed to create or delete output directory");
     info!("Starting tasks");
     components.into_par_iter()
         .map(|task| task.into_result())
