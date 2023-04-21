b43700f9610f8818c7ae755a38b40d71374bce71
Chris Hennick <hennickc@amazon.com>
2023-04-20 19:28:56 -0700

    Eliminate an unused import

--- main/src/image_tasks/repaint.rs
+++ main/src/image_tasks/repaint.rs
@@ -8,7 +8,6 @@ use tracing::instrument;
 
 use crate::image_tasks::color::ComparableColor;
 use crate::image_tasks::make_semitransparent::create_alpha_array;
-use crate::image_tasks::task_spec::CloneableError;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
 pub struct AlphaChannel {
