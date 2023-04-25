90476de5ddde782ea3fd4414bd6c640fce72ad07
Chris Hennick <hennickc@amazon.com>
2023-04-25 09:11:21 -0700

    Restore deleted test that's still useful

--- main/src/image_tasks/repaint.rs
+++ main/src/image_tasks/repaint.rs
@@ -1,5 +1,5 @@
 use log::info;
-use tiny_skia::{Mask, Paint, Pixmap};
+use tiny_skia::{Mask, MaskType, Paint, Pixmap};
 use tiny_skia_path::{Rect, Transform};
 use tracing::instrument;
 use crate::anyhoo;
@@ -22,4 +22,36 @@ pub fn paint(input: &Mask, color: ComparableColor) -> Result<Box<Pixmap>, Clonea
                      Some(input));
     info!("Finishing task: paint with color {}", color);
     Ok(Box::new(output))
+}
+
+#[test]
+fn test_paint() {
+    use tiny_skia::{ColorU8, FillRule, Paint};
+    use tiny_skia_path::{PathBuilder, Transform};
+
+
+    let side_length = 128;
+    let pixmap = &mut Pixmap::new(side_length, side_length).unwrap();
+    let circle = PathBuilder::from_circle(64.0, 64.0, 50.0).unwrap();
+    pixmap.fill_path(&circle, &Paint::default(),
+                     FillRule::EvenOdd, Transform::default(), None);
+    let alpha_channel = Mask::from_pixmap(pixmap.as_ref(), MaskType::Alpha);
+    let repainted_alpha: u8 = 0xcf;
+    let red = ColorU8::from_rgba(0xff, 0, 0, repainted_alpha);
+    let repainted_red: Box<Pixmap> = paint(&alpha_channel, ComparableColor::from(red))
+        .unwrap();
+    let pixmap_pixels = pixmap.pixels();
+    let repainted_pixels = repainted_red.pixels();
+    for index in 0usize..((side_length * side_length) as usize) {
+        let expected_alpha: u8 = (u16::from(repainted_alpha)
+            * u16::from(pixmap_pixels[index].alpha()) / 0xff) as u8;
+        assert!(repainted_pixels[index].alpha().abs_diff(expected_alpha) <= 1);
+        if expected_alpha > 0 {
+            // premultiplied
+            assert_eq!(repainted_pixels[index].red(), repainted_pixels[index].alpha());
+
+            assert_eq!(repainted_pixels[index].green(), 0);
+            assert_eq!(repainted_pixels[index].blue(), 0);
+        }
+    }
 }
