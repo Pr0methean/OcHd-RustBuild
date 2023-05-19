d728f5a8bddc374bf9f439041dd75fd8425133a7
Chris Hennick <hennickc@amazon.com>
2023-05-19 15:22:43 -0700

    Bug fix

--- main/src/image_tasks/from_svg.rs
+++ main/src/image_tasks/from_svg.rs
@@ -4,7 +4,6 @@ use log::info;
 
 use resvg::tiny_skia::{Pixmap, Transform};
 use resvg::usvg::{Options, Tree, TreeParsing};
-use tiny_skia::Rect;
 
 use crate::anyhoo;
 use crate::image_tasks::{allocate_pixmap_empty, MaybeFromPool};
@@ -38,12 +37,13 @@ pub fn from_svg(path: &PathBuf, width: u32) -> Result<MaybeFromPool<Pixmap>,Clon
     let path_str = path.to_string_lossy();
     info!("Starting task: from_svg({})", path_str);
     let svg = SVG_DIR.get_file(path).ok_or(anyhoo!(format!("File not found: {}", path_str)))?;
-    let svg_tree = Tree::from_data(svg.contents(), &Options::default())?;
+    let svg_tree = resvg::Tree::from_usvg(
+        &Tree::from_data(svg.contents(), &Options::default())?);
     let view_box = svg_tree.view_box;
     let height = f64::from(width) * view_box.rect.height() / view_box.rect.width();
-    let scale = (width as f64 / view_box.rect.width()) as f32;
-    let mut out = allocate_pixmap_empty(width, height as u32);
-    resvg::Tree::from_usvg(&svg_tree).render(
+    let scale = (width as f64 / svg_tree.size.width()) as f32;
+    let mut out = allocate_pixmap_empty(width as u32, height as u32);
+    svg_tree.render(
         Transform::from_scale(scale, scale),
         &mut out.as_mut());
     info!("Finishing task: from_svg({})", path_str);
