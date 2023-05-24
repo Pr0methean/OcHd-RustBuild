538f6744c69314a752e794ef527bde4d8e41777a
Chris Hennick <hennickc@amazon.com>
2023-05-24 15:29:10 -0700

    Add clock textures

---
+++ main/src/materials/item/clock.rs
@@ -0,0 +1,24 @@
+use crate::image_tasks::task_spec::{FileOutputTaskSpec, from_svg_task, out_task};
+use crate::stack;
+use crate::texture_base::material::Material;
+
+const CLOCK_ANGLES: usize = 64;
+
+pub struct Clock {}
+
+impl Material for Clock {
+    fn get_output_tasks(&self) -> Vec<FileOutputTaskSpec> {
+        let frame = from_svg_task("clockFrame");
+        let mut output_tasks = Vec::with_capacity(CLOCK_ANGLES);
+        for angle in 0..CLOCK_ANGLES {
+            output_tasks.push(out_task(
+                &format!("item/clock_{:0>2}", angle),
+                stack!(
+                    from_svg_task(&format!("clockDial{}", angle)),
+                    frame.to_owned()
+                )
+            ));
+        }
+        output_tasks
+    }
+}
--- main/src/materials/item/mod.rs
+++ main/src/materials/item/mod.rs
@@ -1,10 +1,12 @@
 mod simple_items;
 mod music_disc;
 mod compass;
+mod clock;
 
 use crate::group;
+use crate::materials::item::clock::Clock;
 use crate::materials::item::compass::COMPASSES;
 use crate::materials::item::music_disc::MUSIC_DISCS;
 use crate::materials::item::simple_items::{SIMPLE_ITEMS};
 
-group!(ALL_ITEMS = SIMPLE_ITEMS, MUSIC_DISCS, COMPASSES);
+group!(ALL_ITEMS = SIMPLE_ITEMS, MUSIC_DISCS, COMPASSES, Clock);
