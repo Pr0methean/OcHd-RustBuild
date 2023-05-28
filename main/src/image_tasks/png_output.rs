a82af6f9f8c179eac15f16adfc9617f5b7d46ded
Chris Hennick <hennickc@amazon.com>
2023-05-28 14:24:33 -0700

    Use Oxipng preset 4

--- main/src/image_tasks/png_output.rs
+++ main/src/image_tasks/png_output.rs
@@ -42,7 +42,7 @@ lazy_static!{
         Vec::clear
     ));
     static ref OXIPNG_OPTIONS: Options = {
-        let mut options = Options::from_preset(6);
+        let mut options = Options::from_preset(4);
         options.deflate = Deflaters::Zopfli {iterations: 30.try_into().unwrap()};
         options.optimize_alpha = true;
         options
