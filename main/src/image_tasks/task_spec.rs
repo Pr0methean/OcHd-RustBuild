58e38f3447a79bdee1237753872ff9e105cb230e
Chris Hennick <hennickc@amazon.com>
2023-06-01 21:04:15 -0700

    Increase discrepancy tolerance to 5 pending investigation

--- main/src/image_tasks/task_spec.rs
+++ main/src/image_tasks/task_spec.rs
@@ -558,9 +558,9 @@ impl PngMode {
                     }
                     if !written {
                         for (index, color) in palette.iter_mut().enumerate() {
-                            if color.red().abs_diff(pixel_color.red()) <= 3
-                                && color.green().abs_diff(pixel_color.green()) <= 3
-                                && color.blue().abs_diff(pixel_color.blue()) <= 3
+                            if color.red().abs_diff(pixel_color.red()) <= 5
+                                && color.green().abs_diff(pixel_color.green()) <= 5
+                                && color.blue().abs_diff(pixel_color.blue()) <= 5
                                 && color.alpha().abs_diff(pixel_color.alpha()) <= 1 {
                                 if *color != pixel_color {
                                     warn!("Rounding discrepancy: expected {}, found {}",
