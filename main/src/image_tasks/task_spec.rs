827f815fbc6305abfd1352bd07e5e76edda2661e
Chris Hennick <hennickc@amazon.com>
2023-06-14 12:46:52 -0700

    Refactor: use actual colors if image is grid-perfect and possible color list is too large

--- main/src/image_tasks/task_spec.rs
+++ main/src/image_tasks/task_spec.rs
@@ -611,7 +611,7 @@ impl ColorDescription {
                         combined_colors.extend(neighbor_colors);
                         combined_colors.sort();
                         combined_colors.dedup();
-                        Self::collapse_specified(combined_colors)
+                        SpecifiedColors(combined_colors)
                     }
                 }
             }
@@ -801,8 +801,6 @@ fn color_description_to_mode(task: &ToPixmapTaskSpec, ctx: &mut TaskGraphBuildin
         Rgb(AlphaChannel) => (RGBA, Eight)
     }
 }
-
-const BINARY_SEARCH_THRESHOLD: usize = 1024;
 
 fn contains_alpha(vec: &Vec<ComparableColor>, needle_alpha: u8) -> bool {
 
@@ -1024,11 +1022,16 @@ impl ToPixmapTaskSpec {
             let alphas = match colors.transparency() {
                 AlphaChannel => match colors {
                     Rgb(_) => ALL_U8S.to_vec(),
-                    SpecifiedColors(vec) => {
-                        let mut alphas: Vec<u8> = vec.into_iter().map(|color| color.alpha()).collect();
+                    SpecifiedColors(colors) => if colors.len() <= BINARY_SEARCH_THRESHOLD {
+                        let mut alphas: Vec<u8> = colors.into_iter().map(|color| color.alpha()).collect();
                         alphas.sort();
                         alphas.dedup();
                         alphas
+                    } else {
+                        ALL_U8S.iter()
+                            .filter(|alpha| contains_alpha(&colors, **alpha))
+                            .copied()
+                            .collect()
                     }
                 },
                 BinaryTransparency => vec![0, u8::MAX],
