2ba0f4174fc43ee662a34dbf4d81afa94804a94b
Chris Hennick <hennickc@amazon.com>
2023-06-14 09:47:17 -0700

    Refactor: reuse scanline buffer

--- main/src/image_tasks/upscale.rs
+++ main/src/image_tasks/upscale.rs
@@ -1,3 +1,4 @@
+use std::iter::repeat;
 use resvg::tiny_skia::{Pixmap};
 use crate::image_tasks::{allocate_pixmap_for_overwrite, MaybeFromPool};
 use crate::image_tasks::task_spec::CloneableError;
@@ -5,15 +6,20 @@ use crate::image_tasks::task_spec::CloneableError;
 pub fn upscale_image(source: &Pixmap, new_width: u32) -> Result<MaybeFromPool<Pixmap>, CloneableError> {
     let scale_factor = new_width / source.width();
     let new_height = scale_factor * source.height();
+    let mut out_scanline = Vec::with_capacity(new_width as usize);
     let mut out = allocate_pixmap_for_overwrite(new_width, new_height);
     for y in 0..source.height() {
+        out_scanline.clear();
         for x in 0..source.width() {
-            let first_pixel = (source.width() * y + x) as usize;
-            for row in 0..scale_factor as usize {
-                let row_start = first_pixel + row * source.width() as usize;
-                out.pixels_mut()[row_start..row_start + scale_factor as usize]
-                    .fill(source.pixel(x, y).unwrap());
-            }
+            out_scanline.extend(repeat(source.pixel(x, y).unwrap())
+                .take(scale_factor as usize));
+        }
+        let start_out_y = (y * scale_factor) as usize;
+        let end_out_y = start_out_y + scale_factor as usize;
+        for out_y in start_out_y..end_out_y {
+            let start_scanline = out_y * new_width as usize;
+            let end_scanline = start_scanline + new_width as usize;
+            out.pixels_mut()[start_scanline..end_scanline].copy_from_slice(&out_scanline);
         }
     }
     Ok(out)
