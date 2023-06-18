2c3e8b01a1238b000de286ff5f05189a4f037e6e
Chris Hennick <hennickc@amazon.com>
2023-06-18 08:30:58 -0700

    Put command blocks first among blocks

--- main/src/materials/block/mod.rs
+++ main/src/materials/block/mod.rs
@@ -9,6 +9,7 @@ pub(crate) mod pickaxe;
 mod shears;
 mod shovel;
 
-group!(ALL_BLOCKS = axe::AXE_BLOCKS, bare_hand::BARE_HAND_BLOCKS, hoe::HOE_BLOCKS,
-        indestructible::INDESTRUCTIBLE_BLOCKS, liquid::LIQUID_BLOCKS, pickaxe::PICKAXE_BLOCKS,
+group!(ALL_BLOCKS = indestructible::INDESTRUCTIBLE_BLOCKS, axe::AXE_BLOCKS,
+        bare_hand::BARE_HAND_BLOCKS, hoe::HOE_BLOCKS,
+        liquid::LIQUID_BLOCKS, pickaxe::PICKAXE_BLOCKS,
         shears::SHEAR_BLOCKS, shovel::SHOVEL_BLOCKS);
